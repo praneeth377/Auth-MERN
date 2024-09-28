fb6b4703114e03d0b80fdcb075a8b0dbba7c26c7
praneeth377 <your-email@example.com>
2024-09-28 18:42:05 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,11 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Failed to connect to MongoDB', err));
 
 const app = express();
 const port = 3000;
